18a3fd2dda2e0f3ba67e9cddc799ffd1057bc5cd
Stephanie Hayward Johns <hayw0055@algonquinlive.com>
2017-01-23 11:30:30 -0500

    added work

--- js/week2.js
+++ js/week2.js
@@ -1,1 +1,54 @@
-alert ("hello");
+
+// task 1 
+var a = 3;
+var b = 5;
+var c;
+
+alert("var a =3;\nvar b = 5;\nvar c;\n----------\n\n" +
+    "a + b = " + (a + b) + "\n" +
+    "a - b = " + (a - b) +"\n" +
+    "a * b = " + (a * b) + "\n" +
+    "b / a = " + (a / b) + "\n" +
+    "b % a = " + (b % a) + "\n" +
+    "a += b" + (a += 1) + "\n" +
+    "a -= b" + (a -= 1) + "\n" +
+    "a *= b" + (a *= b) + "\n" +
+    "a /= b" + (a /= b) + "\n" +
+    "a %= b" + (a %= b) + "\n" +
+    "a == b" + (a == b) + "\n" +
+    "a != b" + (a != b) + "\n" +
+    "a > b" + (a > b) + "\n" +
+    "a < b" + (a < b) + "\n" +
+    "!a && !c " + (!a && !c ) + "\n" +
+    "!a || !c" + (!a || !c) + "\n" );
+      
+
+
+// task 2
+
+
+var first_name = "Stephanie";
+var last_name = "Hayward Johns";
+var email ="hayw0055@algonquinlive.com";
+var output = "My name is " + first_name + " " + last_name + " . You can contact me at " + email + " . " ;
+alert (output);
+
+
+
+
+
+
+// task 3   
+ var numbers = [
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100),
+];
+console.log (number[0] + " + " numbers[4] + " = " + (numbers [0] + numbers[4]));
+
+//check if the number is even or odd
+
+5 % 2 = 1 // means a number is odd
+6 % 2 = 0 // means a number is even 
